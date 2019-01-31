e737b96b7c9eba15283275aad15efe0467bd3b10
jinb1989 <459194288@qq.com>
2019-01-31 09:08:18 +0800

    搭框架

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -3,15 +3,10 @@
     <h1>404! NOT FOUND!</h1>
   </div>
 </template>
-<script>
- 
-</script>
 
 <style lang="scss">
-  .xfn-notfound{
-    text-align:center;
-    padding-top:200px;
-    
+  .xfn-notfound {
+    text-align: center;
+    padding-top: 200px;
   }
 </style>
-
